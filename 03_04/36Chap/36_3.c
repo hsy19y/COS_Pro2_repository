35fb59b6de9d05d1a7996355b99d8f757a630ef9
hsy19y <118896805+hsy19y@users.noreply.github.com>
2023-03-06 00:20:31 +0900

    Update 36_3.c

--- 03_04/36Chap/36_3.c
+++ 03_04/36Chap/36_3.c
@@ -2,17 +2,14 @@
 
 int main()
 {
-    char* s1 = "Hello";
+    char s1[10];
+	char s2[10];
 
-    printf("%c\n", s1[1]);   
-    printf("%c\n", s1[4]);   
-    printf("%c\n", s1[5]);    
+	printf("문자열을 입력하시오: ");
+	scanf("%s %s", s1, s2);
 
-    s1[0] = 'A';           
- 
+	printf("%s\n", s1);
+	printf("%s\n", s2);
 
-    printf("%c\n", s1[0]);
-
-  
-    return 0;
+	return 0;
 }
